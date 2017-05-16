e9066d0eded0ed8e7708797d2c09727568b02846
qiaodan <qiaodandan@fang.com>
2017-05-16 15:57:02 +0800

    microservice-account-wallet的swagger-ui和Eureka注册成功

--- microservice-account-wallet/src/main/java/com/qiaodan/itmei/MicroserviceAccountWalletApplication.java
+++ microservice-account-wallet/src/main/java/com/qiaodan/itmei/MicroserviceAccountWalletApplication.java
@@ -1,9 +1,15 @@
 package com.qiaodan.itmei;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@MapperScan("com.qiaodan.itmei.*")
+@EnableSwagger2
+@EnableEurekaClient
 public class MicroserviceAccountWalletApplication {
 
 	public static void main(String[] args) {
--- microservice-account-wallet/src/main/java/com/qiaodan/itmei/controller/TestController.java
+++ microservice-account-wallet/src/main/java/com/qiaodan/itmei/controller/TestController.java
@@ -3,6 +3,7 @@ package com.qiaodan.itmei.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(value = "wallet的TestController",description = "test controller")
 public class TestController {
 //
-    @RequestMapping("getTestMessage")
+    @RequestMapping(value = "getTestMessage",method = RequestMethod.GET)
     public String getTestMessage(String message){
         return "Hello "+message;
     }
