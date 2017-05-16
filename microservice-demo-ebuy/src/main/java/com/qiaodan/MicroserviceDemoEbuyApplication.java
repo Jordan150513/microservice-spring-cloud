d8231ab628746a7110a15eea053ffda222c07abb
qiaodan <qiaodandan@fang.com>
2017-05-16 12:59:39 +0800

    服务注册到Eureka上成功了

--- microservice-demo-ebuy/src/main/java/com/qiaodan/MicroserviceDemoEbuyApplication.java
+++ microservice-demo-ebuy/src/main/java/com/qiaodan/MicroserviceDemoEbuyApplication.java
@@ -3,11 +3,13 @@ package com.qiaodan;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @MapperScan("com.qiaodan.*")
 @SpringBootApplication
 @EnableSwagger2
+@EnableEurekaClient
 public class MicroserviceDemoEbuyApplication {
 
 	public static void main(String[] args) {
