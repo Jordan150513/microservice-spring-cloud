c65de3331420bc96296098913b83453d5139a37f
qiaodan <qiaodandan@fang.com>
2017-05-22 11:27:42 +0800

    配置小熊 http://localhost:8877/hystrix 小熊出来了 ，剩余是监控的数据 hystrix.stream

--- microservice-consumer-feign/src/main/java/com/qiaodan/itmei/MicroserviceConsumerFeignApplication.java
+++ microservice-consumer-feign/src/main/java/com/qiaodan/itmei/MicroserviceConsumerFeignApplication.java
@@ -5,11 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableHystrixDashboard
+//@EnableHystrix
 public class MicroserviceConsumerFeignApplication {
 
 	public static void main(String[] args) {
