37fb318e28f126b6ac7d582b2bf57bbcf058a0dd
qiaodan <qiaodandan@fang.com>
2017-06-09 11:13:19 +0800

    注释说明

--- doc/select语句.sql
+++ doc/select语句.sql
@@ -1,16 +1,25 @@
 use qdDemo;
+
+-- 用户表
 select * from userinfo;
 
+-- 店铺表
 select * from shop;
 
-
+-- 商品简介表
 select * from goodsBrief;
 
+
+-- 商品详情表
 select * from goodsDetail;
 
+-- 收获地址表
+select * from receiveraddress;
 
+-- 购买记录表
 select * from purchaserecord;
 
+-- 商品图片表
 select * from goodPicture;
 
 
