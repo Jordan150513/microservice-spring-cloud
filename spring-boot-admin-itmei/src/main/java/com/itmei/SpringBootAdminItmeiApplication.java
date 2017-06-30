1d0f5986fa586ea74a91e2581bc5f38704af0fff
qiaodan <qiaodandan@fang.com>
2017-06-30 18:01:43 +0800

    尝试加入admin监控，但是服务一直没有注册到admin。。。。

---
+++ spring-boot-admin-itmei/src/main/java/com/itmei/SpringBootAdminItmeiApplication.java
@@ -0,0 +1,20 @@
+package com.itmei;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@EnableAdminServer
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@Configuration
+public class SpringBootAdminItmeiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminItmeiApplication.class, args);
+	}
+}
