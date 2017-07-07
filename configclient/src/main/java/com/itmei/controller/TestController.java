657c62039722195a4db8a33c8a841fbeb245ee7a
qiaodan <qiaodandan@fang.com>
2017-07-07 15:54:58 +0800

    坑死了，就因为configclient端少写了一个http头，导致client端一直不能去请求成功server端的指向的仓库的配置

--- configclient/src/main/java/com/itmei/controller/TestController.java
+++ configclient/src/main/java/com/itmei/controller/TestController.java
@@ -2,6 +2,7 @@ package com.itmei.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,7 +15,7 @@ public class TestController {
     @Value("${name}")
     private String name;
 
-    @RequestMapping("fetchName")
+    @RequestMapping(value = "fetchName",method = RequestMethod.GET)
     public String fetchName(){
      return this.name;
     }
