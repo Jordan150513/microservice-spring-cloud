cd15ad397008a908c45fbd9b7092ed0cd54121a1
qiaodan <1120465896@qq.com>
2017-07-08 14:33:43 +0800

    demo exercise learning

---
+++ javalearning/src/main/java/com/itmei/JavalearningApplication.java
@@ -0,0 +1,12 @@
+package com.itmei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavalearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavalearningApplication.class, args);
+	}
+}
---
+++ javalearning/src/test/java/com/itmei/JavalearningApplicationTests.java
@@ -0,0 +1,54 @@
+package com.itmei;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavalearningApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+
+	@Test
+	public void seperateWithRegu(){
+
+//		'1231231321'.replace(/(\d)(?=(\d{4})+(?!\d))/g, '$1' + ',');
+		String string = "123456789";
+		// 二狗给的最原始版本的正则表达式 js的语法
+//		String pattern = "/(\\d)(?=(\\d{4})+(?!\\d))/g";
+		// 正向预查 反向预查
+		String pattern = "(\\d)(?=(\\d{3})+(?!\\d))";
+		String str1 = "123456789";
+		System.out.print(str1.replaceAll(pattern, "$1" + ","));
+
+	}
+
+	@Test
+	public void testGroup(){
+		// 按指定模式在字符串查找
+		String line = "This order was placed for QT3000! OK?";
+		String pattern = "(\\D*)(\\d+)(.*)";
+
+		// 创建 Pattern 对象
+		Pattern r = Pattern.compile(pattern);
+
+		// 现在创建 matcher 对象
+		Matcher m = r.matcher(line);
+		if (m.find( )) {
+			System.out.println("Found value: " + m.group(0) );
+			System.out.println("Found value: " + m.group(1) );
+			System.out.println("Found value: " + m.group(2) );
+			System.out.println("Found value: " + m.group(3) );
+		} else {
+			System.out.println("NO MATCH");
+		}
+	}
+}
