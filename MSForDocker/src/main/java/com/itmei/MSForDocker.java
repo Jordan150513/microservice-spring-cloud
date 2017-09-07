23637c42f7b5d6fb53e1f7d6867c79e631bd93e3
qiaodan <qiaodandan@fang.com>
2017-09-07 18:15:52 +0800

    添加docker的测试项目

---
+++ MSForDocker/src/main/java/com/itmei/MSForDocker.java
@@ -0,0 +1,12 @@
+package com.itmei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MSForDocker {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MSForDocker.class, args);
+	}
+}
