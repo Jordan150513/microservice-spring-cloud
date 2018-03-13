08fb9467149b7de6aebfd25e599320001a93f25f
qiaodan <qiaodandan@fang.com>
2018-03-13 21:09:13 +0800

    public int romanToInt(String s)  罗马数字转换成 int数字 初步版本

--- javalearning/src/test/java/com/itmei/leetcode/Solution.java
+++ javalearning/src/test/java/com/itmei/leetcode/Solution.java
@@ -2,6 +2,9 @@ package com.itmei.leetcode;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by qiaodan on 2018/3/12.
  */
@@ -125,4 +128,64 @@ public class Solution {
         int a = KMP(str, 36, ptr, 7);
         System.out.println(a);
     }
+    // I（1）、V（5）、X（10）、L（50）、C（100）、D（500）和M（1000）
+        /*
+        * 1、重复数次：一个罗马数字重复几次，就表示这个数的几倍。
+2、右加左减：
+2.1 在较大的罗马数字的右边记上较小的罗马数字，表示大数字加小数字。
+2.2 在较大的罗马数字的左边记上较小的罗马数字，表示大数字减小数字。
+2.3 左减的数字有限制，仅限于I、X、C。比如45不可以写成VL，只能是XLV
+2.4 但是，左减时不可跨越一个位数。比如，99不可以用IC（100 - 1）表示，而是用XCIX（[100 - 10] + [10 - 1]）表示。（等同于阿拉伯数字每位数字分别表示。）
+2.5 左减数字必须为一位，比如8写成VIII，而非IIX。
+2.6 右加数字不可连续超过三位，比如14写成XIV，而非XIIII。（见下方“数码限制”一项。）
+
+3、加线乘千：
+3.1 在罗马数字的上方加上一条横线或者加上下标的Ⅿ，表示将这个数乘以1000，即是原数的1000倍。
+3.2 同理，如果上方有两条横线，即是原数的1000000（1000^{2}）倍。
+
+4、数码限制：
+4.1 同一数码最多只能出现三次，如40不可表示为XXXX，而要表示为XL。
+4.2 例外：由于IV是古罗马神话主神朱庇特（即IVPITER，古罗马字母里没有J和U）的首字，因此有时用IIII代替
+*/
+
+    public int romanToInt(String s) {
+
+        HashMap map = new HashMap();
+        map.put("I",1);
+        map.put("V",5);
+        map.put("X",10);
+        map.put("L",50);
+        map.put("C",100);
+        map.put("D",500);
+        map.put("M",1000);
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        for (int i=0;i<s.length();i++){
+            String key = s.substring(i,i+1);
+            if (key.equals("I")||key.equals("X")||key.equals("C")){
+                // 左减的数字有限制，仅限于I、X、C
+                if (i>0){
+                    String keypre = s.substring(i-1,i);
+                    if(new Integer(map.get(keypre).toString()).intValue()<new Integer(map.get(key).toString()).intValue()){
+                        int value = -(new Integer(map.get(keypre).toString())).intValue();
+                        arrayList.set(i-1,value);
+                    }
+                }
+            }
+            int value = (new Integer(map.get(key).toString())).intValue();
+            arrayList.add(i,value);
+        }
+        int rs = 0;
+        for (int i=0;i<arrayList.size();i++){
+            rs = rs+arrayList.get(i);
+        }
+        return rs;
+    }
+
+    @Test
+    public void  testRomanToInt(){
+
+        int rs = romanToInt("DCXXI");
+        System.out.println(rs);
+    }
+
 }
